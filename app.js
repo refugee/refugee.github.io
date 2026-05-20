7750cd4aa6a92dbf4bf623067e458806e2f2b8c3
refugee <37581643+refugee@users.noreply.github.com>
2026-05-20 15:27:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const SETTINGS = {
   whatsappDisplay: '724 366 369',
   hero: 'images/hero.jpg',
   // Sem vlož URL Google Apps Script Web Appu po nasazení.
-  orderApiUrl: 'https://script.google.com/macros/s/AKfycbwgLyGIKOQkF-FC-xCY9-kzyVCfP6pvBVr_s7epXwwosY9M45X6TBKAlVPKEVPrOIgL/exec, 
+  orderApiUrl: 'https://script.google.com/macros/s/AKfycbxf2BKxNqCAMwb2DkC3jAuHZd1O_QY0wVym2uix4xt8i8HgFB4V1K7tymUE49KeDO-_/exec, 
   wines: [
     {
       id: 1,
